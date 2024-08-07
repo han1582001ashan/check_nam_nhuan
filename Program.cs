026de12b62b31ad3bc001652028d29d182b04b3d
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-07 21:23:36 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CheckLeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter a year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0)
+                {
+                    Console.WriteLine(year + " is a leap year");
+                }
+                else
+                {
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine(year + " is a leap year");
+                    }
+                    else
+                    {
+                        Console.WriteLine(year + " is NOT a leap year");
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(year + " is NOT a leap year");
+            }
+
+        }
+    }
+}
